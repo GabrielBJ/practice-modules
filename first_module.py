2e04088f883b985f4805d33a69a6a36ef6bf3fe4
GabrielBJ <gabrielbala2001@gmail.com>
2023-10-29 20:38:17 -0500

    update

--- first_module.py
+++ first_module.py
@@ -12,3 +12,31 @@ class car:
         self.distance = distance
         self.fuel_meter = fuel_meter
 
+"wee need to ad:d this line to run the module as a script"
+
+# Here we are goin to define a new class 
+class car:
+
+    def __init__(self, name, max_speed, distance, fuel_meter):
+        self.name = name
+        self.max_speed = max_speed
+        self.distance = distance
+        self.fuel_meter = fuel_meter
+
+    def drive(self):
+        print("driving")
+
+    def stop(self):
+        print("stopped")
+
+    def park(self):
+        print("parking")
+
+    def refuel(self):
+        print("refueling")
+
+    def status(self):
+        print("name: ", self.name)
+        print("max_speed: ", self.max_speed)
+        print("distance: ", self.distance)
+        print("fuel_meter: ", self.fuel_meter)
