91f43094dcb985e6066e91777c5149610a981a77
GabrielBJ <gabrielbala2001@gmail.com>
2023-11-02 01:28:40 -0500

    update

--- first_module.py
+++ first_module.py
@@ -1,42 +1,10 @@
-#!usr/bin/env python3
+#!/usr/bin/ python3
+import numpy as np
+import pandas as pd
 
-def hello():
-    print("First module ")
+def my_function(b):
+    a = np.cos(b) + 1
+    
+    return a
 
 
-class car:
-
-    def __init__(self, name, max_speed, distance, fuel_meter):
-        self.name = name
-        self.max_speed = max_speed
-        self.distance = distance
-        self.fuel_meter = fuel_meter
-
-"wee need to ad:d this line to run the module as a script"
-
-# Here we are goin to define a new class 
-class car:
-
-    def __init__(self, name, max_speed, distance, fuel_meter):
-        self.name = name
-        self.max_speed = max_speed
-        self.distance = distance
-        self.fuel_meter = fuel_meter
-
-    def drive(self):
-        print("driving")
-
-    def stop(self):
-        print("stopped")
-
-    def park(self):
-        print("parking")
-
-    def refuel(self):
-        print("refueling")
-
-    def status(self):
-        print("name: ", self.name)
-        print("max_speed: ", self.max_speed)
-        print("distance: ", self.distance)
-        print("fuel_meter: ", self.fuel_meter)
